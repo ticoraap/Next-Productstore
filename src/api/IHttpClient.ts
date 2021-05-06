329074f986f391f3d6897390b08f7173b173c035
Tico Raaphorst <>
2021-05-06 13:59:08 +0200

    removed firebase

--- src/api/IHttpClient.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from "axios";
-import { IProduct } from "../models/product";
-
-export interface IHttpClient {
-    get<T = unknown>(url: string): Promise<IProduct>
-    getAll<T = unknown>(url: string): Promise<IProduct[]>;
-    getAllIds<T = unknown>(url: string): Promise<string[]>
-    add<T = unknown>(url: String, data: IProduct): Promise<T>;
-}
